ced7b76fe33f76dc6db395a1aa335b3f168877e2
Jeferson Oliveira <88509360+jefersonolivaras@users.noreply.github.com>
2025-12-22 16:49:23 +0000

    alteração de frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Teste</h1>
+    return <h1>Mais uma vez na luta, na última boa luta que jamais conhecerei. Viver e morrer, neste dia. Viver e morrer, neste dia.</h1>
 }
 
 export default Home;
