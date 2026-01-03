68a72f0135e7f73ed35eceb6197206dfa6a8e0e0
Jeferson Oliveira <88509360+jefersonolivaras@users.noreply.github.com>
2026-01-03 01:50:56 +0000

    Modifica pontuação

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Mais uma vez na luta, na última boa luta que jamais conhecerei. Viver e morrer, neste dia. Viver e morrer, neste dia.</h1>
+    return <h1>Mais uma vez na luta, na última boa luta que jamais conhecerei. Viver e morrer, neste dia. Viver e morrer... neste dia.</h1>
 }
 
 export default Home;
